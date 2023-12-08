69b41eed3f45c8d344746e431a6c54c965ad2465
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-12-09 02:11:55 +0600

    Add
    Add

---
+++ HomeTest/HomeTest/src/main/java/tttm/com/HomeTest/HomeTestApplication.java
@@ -0,0 +1,13 @@
+package tttm.com.HomeTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeTestApplication.class, args);
+	}
+
+}
---
+++ HomeTest/HomeTest/src/test/java/tttm/com/HomeTest/HomeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tttm.com.HomeTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
