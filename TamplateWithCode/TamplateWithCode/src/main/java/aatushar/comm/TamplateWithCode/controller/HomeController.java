ead5c6dc63db9c3b2623483c3a8d9e0c11a63aa5
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2023-12-21 01:30:45 +0600

    Add
    Add

--- TamplateWithCode/TamplateWithCode/src/main/java/aatushar/comm/TamplateWithCode/controller/HomeController.java
+++ TamplateWithCode/TamplateWithCode/src/main/java/aatushar/comm/TamplateWithCode/controller/HomeController.java
@@ -1,19 +1,20 @@
 package aatushar.comm.TamplateWithCode.controller;
 
 
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class HomeController {
-//
-//    @GetMapping("/")
-//    public String home(){
-//        return "spring";
-//    }
-//
-//    @GetMapping("/jee")
-//    public String jee(){
-//        return "jee";
-//    }
-//}
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "spring";
+    }
+
+
+    @GetMapping("/header")
+    public String header(){
+        return "header";
+    }
+}
