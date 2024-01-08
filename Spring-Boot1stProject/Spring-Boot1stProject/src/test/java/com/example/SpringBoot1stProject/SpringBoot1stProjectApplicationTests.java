d860c20847ad9b0489bbec9cbda7fef00c23095c
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-01-09 00:34:34 +0600

    add
    add

---
+++ Spring-Boot1stProject/Spring-Boot1stProject/src/test/java/com/example/SpringBoot1stProject/SpringBoot1stProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot1stProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1stProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
