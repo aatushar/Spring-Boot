0e7ee7985d1059f6f0171efcdc9dcc3b7a2e5669
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-01-10 13:30:28 +0600

    Add
    Add

---
+++ RestartProject/src/test/java/com/example/RestartProject/RestartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestartProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
