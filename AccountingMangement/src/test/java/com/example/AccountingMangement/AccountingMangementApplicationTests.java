5f3603a7cd5905bafdaac694f2a56a961e131dce
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-01-11 13:27:41 +0600

    Add
    Add

---
+++ AccountingMangement/src/test/java/com/example/AccountingMangement/AccountingMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccountingMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
