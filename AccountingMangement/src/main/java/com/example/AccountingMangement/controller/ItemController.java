6a7524b1f27cb989f457ab7dcb0d404544688a3c
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-01-14 13:38:13 +0600

    Add
    Add

---
+++ AccountingMangement/src/main/java/com/example/AccountingMangement/controller/ItemController.java
@@ -0,0 +1,21 @@
+package com.example.AccountingMangement.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/item")
+
+public class ItemController {
+    @GetMapping("")
+    public String item(){
+        return "item";
+    }
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+}
