db6a17a0859b388a1a70aa67d409525ff0083b64
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-01-20 00:50:41 +0600

    Add
    Add

--- AccountingMangement/src/main/java/com/example/AccountingMangement/controller/ItemController.java
+++ AccountingMangement/src/main/java/com/example/AccountingMangement/controller/ItemController.java
@@ -1,23 +1,61 @@
 package com.example.AccountingMangement.controller;
 
 
+import com.example.AccountingMangement.model.Item;
+import com.example.AccountingMangement.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 
 
 
-@RequestMapping("/item")
+@RequestMapping("/item/")
 public class ItemController {
-    @GetMapping("")
-    public String item(){
-        return "item";
-    }
-//    @GetMapping("/index")
-//    public String index(){
-//        return "index";
-//    }
+    @Autowired
+    private ItemService service;
 
+    @GetMapping("/")
+    public String getAllItem(Model m) {
+        List<Item> itemList = service.getAllItem();
+        m.addAttribute("itemList", itemList);
+        m.addAttribute("title", "All Item");
+        return "viewItem";
+    }
+
+    @GetMapping("/additem")
+    public String addForm(Model m) {
+
+        m.addAttribute("item", new Item());
+        m.addAttribute("title", "Add Item");
+        return "additem";
+
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String addItem(@ModelAttribute Item i) {
+        service.save(i);
+        return "redirect:/item/";
+
+
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable int id) {
+
+        service.deleteItem(id);
+        return "redirect:/item/";
+    }
+    @GetMapping("/edit/{id}")
+    public String editItem(@PathVariable int id, Model m) {
+
+        Item i = service.editItem(id);
+        m.addAttribute("item", i);
+
+        return "additem";
+    }
 }
