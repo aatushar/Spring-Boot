94fefb68cdeedb5c806c3a427be15438d0d097e8
Md. Abdul Alim Tushar <132429007+aatushar@users.noreply.github.com>
2024-02-11 13:34:07 +0600

    Add
    Add

--- AccountingMangementBySir/AccountingMangement/src/main/java/com/example/AccountingMangement/repository/TestJournalRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.AccountingMangement.repository;
-
-import com.example.AccountingMangement.model.TestJournal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestJournalRepo extends JpaRepository<TestJournal, Long> {
-}
